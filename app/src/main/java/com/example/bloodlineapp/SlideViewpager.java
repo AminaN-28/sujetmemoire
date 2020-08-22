a4341521f3e10b2887811adf4df0d4121e5b0f45
Aminata Ndiaye <aminatandiaye44@gmail.com>
2020-08-23 01:58:31 +0200

    Projet Memoire

---
+++ app/src/main/java/com/example/bloodlineapp/SlideViewpager.java
@@ -0,0 +1,183 @@
+package com.example.bloodlineapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SlideViewpager extends PagerAdapter {
+    Context ctx;
+
+    public SlideViewpager(Context ctx) {
+        this.ctx = ctx;
+    }
+
+
+    @Override
+    public int getCount() { return 5; }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+        LayoutInflater layoutInflater= (LayoutInflater) ctx.getSystemService(ctx.LAYOUT_INFLATER_SERVICE);
+        View view=layoutInflater.inflate(R.layout.activity_main,container,false);
+
+
+        ImageView logo=view.findViewById(R.id.logo);
+
+        ImageView ind1=view.findViewById(R.id.ind1);
+        ImageView ind2=view.findViewById(R.id.ind2);
+        ImageView ind3=view.findViewById(R.id.ind3);
+        ImageView ind4=view.findViewById(R.id.ind4);
+        ImageView ind5=view.findViewById(R.id.ind5);
+        ImageView ind6=view.findViewById(R.id.ind6);
+
+
+        TextView title=view.findViewById(R.id.title);
+        TextView desc=view.findViewById(R.id.desc);
+
+        ImageView next=view.findViewById(R.id.next);
+        ImageView back=view.findViewById(R.id.back);
+
+        Button btnGetStarted=view.findViewById(R.id.button);
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ctx,MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+                ctx.startActivity(intent);
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SlideActivity.viewPager.setCurrentItem(position+1);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SlideActivity.viewPager.setCurrentItem(position-1);
+            }
+        });
+
+        switch (position)
+        {
+            case 0:
+                logo.setImageResource(R.drawable.privatedata);
+                ind1.setImageResource(R.drawable.selected);
+                ind2.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.unselected);
+                ind4.setImageResource(R.drawable.unselected);
+                ind5.setImageResource(R.drawable.unselected);
+                ind6.setImageResource(R.drawable.unselected);
+
+                title.setText("Private Data");
+                desc.setText("Register and Save Your Data.");
+                back.setVisibility(View.GONE);
+                next.setVisibility(View.VISIBLE);
+                break;
+            case 1:
+                logo.setImageResource(R.drawable.location);
+                ind1.setImageResource(R.drawable.unselected);
+                ind2.setImageResource(R.drawable.selected);
+                ind3.setImageResource(R.drawable.unselected);
+                ind4.setImageResource(R.drawable.unselected);
+                ind5.setImageResource(R.drawable.unselected);
+                ind6.setImageResource(R.drawable.unselected);
+
+                title.setText("Location");
+                desc.setText("Give Your Location and find who is near of you.");
+                back.setVisibility(View.VISIBLE);
+                next.setVisibility(View.VISIBLE);
+                break;
+            case 2:
+                logo.setImageResource(R.drawable.socialinteraction);
+                ind1.setImageResource(R.drawable.unselected);
+                ind2.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.selected);
+                ind4.setImageResource(R.drawable.unselected);
+                ind5.setImageResource(R.drawable.unselected);
+                ind6.setImageResource(R.drawable.unselected);
+
+                title.setText("Social Interaction");
+                desc.setText("Interact with other donors or recipients." +
+                        "As much as we complain about other people, there is nothing worse for mental health than a social desert.");
+                back.setVisibility(View.VISIBLE);
+                next.setVisibility(View.GONE);
+
+            case 3:
+                logo.setImageResource(R.drawable.socialinteraction);
+                ind1.setImageResource(R.drawable.unselected);
+                ind2.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.unselected);
+                ind4.setImageResource(R.drawable.selected);
+                ind5.setImageResource(R.drawable.unselected);
+                ind6.setImageResource(R.drawable.unselected);
+
+                title.setText("Make alert");
+                desc.setText("If You need blood donation Make an alert and share it.");
+                back.setVisibility(View.VISIBLE);
+                next.setVisibility(View.GONE);
+
+
+            case 4:
+                logo.setImageResource(R.drawable.answeralert);
+                ind1.setImageResource(R.drawable.unselected);
+                ind2.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.unselected);
+                ind4.setImageResource(R.drawable.unselected);
+                ind5.setImageResource(R.drawable.selected);
+                ind6.setImageResource(R.drawable.unselected);
+
+                title.setText("Answer to alert");
+                desc.setText("React to Alert");
+                back.setVisibility(View.VISIBLE);
+                next.setVisibility(View.GONE);
+
+
+
+            case 5:
+                logo.setImageResource(R.drawable.helpwithlove);
+                ind1.setImageResource(R.drawable.unselected);
+                ind2.setImageResource(R.drawable.unselected);
+                ind3.setImageResource(R.drawable.unselected);
+                ind4.setImageResource(R.drawable.unselected);
+                ind5.setImageResource(R.drawable.unselected);
+                ind6.setImageResource(R.drawable.selected);
+
+                title.setText("Help Others");
+                desc.setText("You have not lived today until you have done something for someone who can never repay you.");
+                back.setVisibility(View.VISIBLE);
+                next.setVisibility(View.GONE);
+
+                break;
+
+
+        }
+
+
+        container.addView(view);
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+}
--- app/src/main/java/com/example/bloodlineapp/WriteAlert.java
+++ app/src/main/java/com/example/bloodlineapp/WriteAlert.java
@@ -1,14 +1,71 @@
 package com.example.bloodlineapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class WriteAlert extends AppCompatActivity {
+
+    EditText text_alert;
+    Button publish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_alert);
+
+
+        text_alert = findViewById(R.id.makerequest);
+        publish=findViewById(R.id.publishbtn);
+
+
+        publish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Creation d'un dictionnaire
+                Map<String,String> map = new HashMap<String, String>();
+                map.put("nom","amina");
+
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue("Hello, World!");
+
+                // Read from the database
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d("280399", "Value is: " + value);
+
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+
+                        // Failed to read value
+                        Log.w("2803","Failed to read value.", error.toException());
+                    }
+                });
+
+            }
+        });
+
     }
 }
